19f89e70e46ec79be4d66d1a947318d942543749
arfadpv <dreamcoder94@gmail.com>
2022-06-14 08:14:31 +0530

    Updates

---
+++ precache-manifest.f58d4fc675eaa0d7944e73afbb921853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c75d1088cdc5d7b8943",
+    "url": "/arfnews/static/css/main.990fbeab.chunk.css"
+  },
+  {
+    "revision": "9c75d1088cdc5d7b8943",
+    "url": "/arfnews/static/js/main.9c75d108.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/arfnews/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "201d3d7b8fe0b9207915",
+    "url": "/arfnews/static/js/2.201d3d7b.chunk.js"
+  },
+  {
+    "revision": "15b736bc11c45c6b4a30",
+    "url": "/arfnews/static/js/runtime~main.15b736bc.js"
+  },
+  {
+    "revision": "95edd7834dcd929952fde548b2c44ea6",
+    "url": "/arfnews/index.html"
+  }
+];
